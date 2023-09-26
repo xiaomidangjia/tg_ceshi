75109f09639df743a182a9a07696907ad12f350c
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2023-09-26 11:03:28 +0800

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -11,8 +11,11 @@ import telegram
 
 bot = telegram.Bot(token='6343206405:AAHkaKIXCMvif0yqkzvTYWasYPEIsTmImgQ')
 
-bot.sendMessage(chat_id='-1001975215255', text='测试',message_thread_id=4) #开仓信息同步
-bot.sendMessage(chat_id='-1001975215255', text='测试',message_thread_id=3) #链上大额转账
-bot.sendMessage(chat_id='-1001975215255', text='测试',message_thread_id=5) #链上数据分享
+try:
+    await bot.initialize()
+    bot.sendMessage(chat_id='-1001975215255', text='测试',message_thread_id=4) #开仓信息同步
+    # code
+finally:
+    await bot.shutdown()
 
  
